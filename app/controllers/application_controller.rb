a01ec3ba6384471b7486f13e1697c84228fe2fe2
M.H <basketball.mero63@gmail.com>
2021-02-15 13:23:54 +0000

    forheroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,6 @@ class ApplicationController < ActionController::API
  
  include ActionController::Cookies
  
- include ActionController::HttpAuthentication::Token::ControllerMethods
-
- before_action :authenticate
- 
- protected
-  def authenticate
-    authenticate_or_request_with_http_token do |token, options|
-      token == 'FOO'
-    end
-  end
  
  private
   
