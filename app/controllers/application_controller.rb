4b4554099cc02f45c68e72d13d60ed8654beab34
M.H <basketball.mero63@gmail.com>
2021-02-16 13:51:09 +0000

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,16 @@ class ApplicationController < ActionController::API
  
  include ActionController::HttpAuthentication::Token::ControllerMethods
 
- before_action :authenticate!
+ #before_action :authenticate!
  
  
  private
  
- def authenticate!
-    authenticate_or_request_with_http_token do |token, options|
-      User.find_by(token: token).present?
-    end
- end
+ #def authenticate!
+    #authenticate_or_request_with_http_token do |token, options|
+      #User.find_by(token: token).present?
+    #end
+ #end
 
   def current_user
     @current_user ||= User.find_by(token: request.headers['Authorization'].split[1])
