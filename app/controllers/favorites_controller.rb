e418fa9f0dc7542c6f8f29dcfb58835fcfb8ed5e
M.H <basketball.mero63@gmail.com>
2021-02-17 06:04:47 +0000

    heroku

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,5 @@
 class FavoritesController < ApplicationController
+  before_action :set_chat, only: [:destroy]
   def create
     chat = Chat.find(params[:chat_id])
     current_user.favorite(chat)
@@ -12,9 +13,15 @@ class FavoritesController < ApplicationController
   #end
   
   def destroy
-    chat = Chat.find(params[:id])
-    current_user.unfavorite.(chat)
+    current_user.unfavorite.(@chat)
     render json: {message: 'unfavorite in success'}
   end
   
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_chat
+      @chat = Chat.find(params[:id])
+    end
+    
 end
